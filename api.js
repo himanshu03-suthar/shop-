a894108e6a3d3b95e3cfb3ca6426231247fe81c8
himanshu03-suthar <hs7882371@gmail.com>
2026-04-25 11:47:18 +0530

    hello babu

---
+++ api.js
@@ -0,0 +1,16 @@
+async function loginUser(){
+    const response = await fetch('https://dummyjson.com/auth/login',  {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            username: 'emilys',
+            password: 'emilyspass',
+            expiresInMins: 30
+        })
+    });
+    const data = await response.json();
+    console.log(data);
+}
+loginUser();
